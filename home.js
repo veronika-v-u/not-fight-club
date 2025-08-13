2f684dd60a35911c4ad5c36226e4395e1ea88c54
Veronika <veronika13072004@gmail.com>
2025-08-13 19:33:09 +0300

    chore: clear main branch to accept PRs from gh-pages

--- home.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.btn-fight').addEventListener('click', () => {
-  const battleId = Date.now();
-  sessionStorage.setItem('currentBattleId', battleId);
-  window.location.href = 'fight.html';
-});
